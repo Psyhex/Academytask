14a1179072d99e60d32f397807766dc0b1590936
Psyhex <rokas.sapola@gmail.com>
2022-11-02 18:44:56 +0200

    Add files via upload

---
+++ task-be/WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ItemController : ControllerBase
+	{
+		private readonly DataContext _context;
+
+		public ItemController(DataContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<Item>>> Get()
+		{
+			return Ok(await _context.Items.ToArrayAsync());
+		}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Item>> Get(int id)
+		{
+			var item = await _context.Items.FindAsync(id);
+			if (item == null)
+				return BadRequest("Item not found");
+			return Ok(item);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<Item>>> AddItem(Item item)
+		{
+			_context.Items.Add(item);
+			await _context.SaveChangesAsync();
+
+			return Ok(await _context.Items.ToListAsync());
+		}
+	}
+}
---
+++ task-be/WebApplication1/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1
+{
+	public class Item
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+	}
+}
